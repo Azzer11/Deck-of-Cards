e909719d2986619a17d7ebef183c89f38dca82cf
Azzer11 <azzer11@icloud.com>
2025-08-10 22:43:29 +1000

    Add files via upload
    This is my first project here.
    I'm trying to create a working system that handles storing a deck of 52 standard playing cards and using them for basic actions like Drawing cards, Playing cards and Discarding cards.
    To see if I could possibly reuse this system in other projects I created a Class Library that has all the class info for the Cards and Decks inside before trying to connect this to the actual console app.
    So far not going well, I'm 99% sure this should be working but it currently doesn't build correctly.
    Error is CS0006 and I'm pretty sure I just connected the ClassLibrary to the ConsoleApp incorrectly somehow, any input would be appreciated.
    Next steps are to get this actually functioning and displaying at least 1 line of text before the more complicated stuff.

---
+++ GitHubTest/CardGameTest/Program.cs
@@ -0,0 +1,9 @@
+﻿//This project should have access to the class library i set up and will be used to execute actual code.
+using Cards_DecksClass;
+
+// Test variables to see if can get get this to display anything before moving forward.
+var Card0 = new Cards();
+Card0.Rank = "Ace";
+Card0.Suit = "Spades";
+
+Card0.Draw();
---
+++ GitHubTest/TestLibrary/Class1.cs
@@ -0,0 +1,43 @@
+﻿using Cards_DecksClass;
+// I think this is where i should start for decks? research suggests lists would be better but idk how those work yet
+// Now i have to figure out how to create 52 cards inside the Array without having to write 100+ lines of code typing them out individually.
+var Deck0 = new Decks[51];
+
+namespace Cards_DecksClass
+{
+    // Each Card has 2 variables, a Rank and a Suit.
+    // Each card has several possible actions, Draw, Discard and Play.
+    // Each card is also stored in a deck but idk how to figure that out yet
+    public class Cards
+    {
+        // we want the Cards to have a numerical value related to their rank, i can do that manually but if i can tie it to the rank string that would be nice.
+        public string Rank { get; set; }
+        public string Suit { get; set; }
+        public string CardInfo => $"{Rank} of {Suit}";
+
+        // figure out how to get user input for these actions. Console.Readline() ? no idea how to get that working between project files.
+        public void Draw()
+        {
+            // figure out how to get a random card from the deck and display that here. also figure out how to remember which card was drawn.
+            Console.WriteLine($"You Draw {CardInfo} from the deck.");
+        }
+        public void Discard()
+        {
+            // figure out how  these things are stored and how to put them into new places
+            // Cards should only be allowed to Discard if you have already drawn them, you should also be allowed to select which card you discard.
+            Console.WriteLine($"You Discard {CardInfo}.");
+        }
+        public void Play()
+        {
+            // figure out how to get the total numerical value of all cards played and display it
+            Console.WriteLine($"You Play the {CardInfo}");
+        }
+    }
+
+    // Decks should hold cards in them for use and keep track of which cards are still in the deck and which cards have been drawn.
+    // They should also hold a full deck of 52 standard playing cards and be able to reshuffle cards back into the deck and redistribute the cards in a random order.
+    public class Decks
+    {
+        //i have no idea where to start here
+    }
+}
