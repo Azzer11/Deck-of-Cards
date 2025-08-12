27997a89122815b60e495d6b4227efe10c6aacdd
Azzer11 <azzer11@icloud.com>
2025-08-12 19:31:05 +1000

    Changed class name from Cards to PlayingCards. Moved Public Void Draw and Public Void Shuffle into the Decks class. This has broken the Card0.Draw and .Shuffle lines. As a temp fix these lines have been commented out. Added a new var to create a populated deck of PlayingCards - currently unsure if done correctly.

--- GitHubTest/CardGameTest/Classes/Decks.cs
+++ GitHubTest/CardGameTest/Classes/Decks.cs
@@ -1,13 +1,37 @@
 ﻿namespace CardGameTest.Classes;
-
 // Decks should hold cards in them for use and keep track of which cards are still in the deck and which cards have been drawn.
 // They should also hold a full deck of 52 standard playing cards and be able to reshuffle cards back into the deck and redistribute the cards in a random order.
 public class Decks
 {
-    //i have no idea where to start here
+        // figure out how to get a random card from the deck and display that here. also figure out how to remember which card was drawn.
+    //public void Draw()
+    //{
+    //    Console.WriteLine($"You Draw {CardInfo} from the deck.");
+    //}
+        // Randomize a list of cards in the deck and pick the top card to draw.
+    //public void Shuffle()
+    //{
+    //    Random random = new Random();
+    //    Rank = random.Next(1, 14) switch
+    //    {
+    //        1 => "Ace",
+    //        11 => "Jack",
+    //        12 => "Queen",
+    //        13 => "King",
+    //        var n => n.ToString()
+    //    };
+    //    Suit = random.Next(0, 4) switch
+    //    {
+    //        0 => "Hearts",
+    //        1 => "Diamonds",
+    //        2 => "Clubs",
+    //        3 => "Spades",
+    //        _ => throw new InvalidOperationException("Invalid suit index")
+    //    };
+    //    Console.WriteLine("The deck has been shuffled.");
+    //}
 
-
-    // A deck should contain a list of cards, and methods to shuffle, draw, and discard cards.
+        // A deck should contain a list of cards, and methods to shuffle, draw, and discard cards.
 
     // Suggest thinking about moving the card logic into the Decks class, as it might be more appropriate to manage the cards collectively rather than individually.
     // Then the card only needs to know about itself
--- GitHubTest/CardGameTest/Program.cs
+++ GitHubTest/CardGameTest/Program.cs
@@ -1,19 +1,23 @@
 ﻿//This project should have access to the class library i set up and will be used to execute actual code.
- using CardGameTest.Classes;
+using CardGameTest.Classes;
+
 // Test variables to see if can get get this to display anything before moving forward.
-var Card0 = new Cards();
+var Card0 = new PlayingCards();
 Card0.Rank = "Ace";
 Card0.Suit = "Spades";
 
+//this should theoretically create a new List of 52 PlayingCards, not really sure though barely understand this, will have to re-research LINQ to understand this
+var NewDeckList = Enumerable.Range(1, 52).Select(Card => new PlayingCards()).ToList();
+Console.WriteLine(NewDeckList);
 
-Card0.Draw();
-Card0.Shuffle();
-Card0.Draw();
-Card0.Shuffle();
-Card0.Draw();
-Card0.Shuffle();
-Card0.Draw();
-Card0.Shuffle();
-Card0.Draw();
- 
+//Card0.Draw();
+//Card0.Shuffle();
+//Card0.Draw();
+//Card0.Shuffle();
+//Card0.Draw();
+//Card0.Shuffle();
+//Card0.Draw();
+//Card0.Shuffle();
+//Card0.Draw();
+
 
